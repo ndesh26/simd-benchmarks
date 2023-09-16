530f5cf2bf6e2cc29162ee9416124dc75ee1b39e
Nayan Deshmukh <nayan26deshmukh@gmail.com>
2023-09-16 02:03:25 +0000

    Add scalar code for numerical integration
    Signed-off-by: Nayan Deshmukh <nayan26deshmukh@gmail.com>

---
+++ numerical-integration/scalar_no_avx.cpp
@@ -0,0 +1,25 @@
+#include <benchmark/benchmark.h>
+
+static float f(float a) { return a * a; }
+
+static float numericalIntegration(float a, float b, int n) {
+    auto dx = (b - a) / n;
+    auto integral = f(a) / 2 + f(b) / 2;
+
+    for (int k = 1; k < n; k++) {
+        integral += f(a + k * dx);
+    }
+
+    integral *= dx;
+    return integral;
+}
+
+static void BM_numericalIntegration_no_avx(benchmark::State& state) {
+    // Perform setup here
+    for (auto _ : state) {
+        // This code gets timed
+        benchmark::DoNotOptimize(numericalIntegration(0, 1, 20000));
+    }
+}
+// Register the function as a benchmark
+BENCHMARK(BM_numericalIntegration_no_avx);
